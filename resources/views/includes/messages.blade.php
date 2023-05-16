d117e010022b34d21a4ab16d1f29321668af5c23
nguyenhy <nguyenhy311@gmail.com>
2023-05-16 09:52:19 +0700

    reproduce issue

---
+++ resources/views/includes/messages.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <span>file: messages.blade</span>
+</div>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <span>links goes here</span>
+    </div>
+</nav>
